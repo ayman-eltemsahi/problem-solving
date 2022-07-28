11daca9e65005edc12f32b3998e76b24ff846fca
Ayman Eltemsahi <aymaneltemsahi@hotmail.com>
2022-07-28 21:11:51 +0200

    moved the headers used locally to a different file

---
+++ cpp/leetcode/local-stuff.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+
+#include "utils/print-vector.hpp"
+#include "utils/read-vector.hpp"
+
+typedef long long int ll;
+typedef std::vector<int> vi;
+using std::abs;
+using std::map;
+using std::max;
+using std::min;
+using std::pair;
+using std::queue;
+using std::set;
+using std::stack;
+using std::string;
+using std::unordered_map;
+using std::unordered_set;
+using std::vector;
+#define FORN(i, n) for (int i = 0; i < (n); i++)
+#define FORN1(i, n) for (int i = 1; i < (n); i++)
+#define LOG(a) std::cout << (a) << "\n"
+#define LOG2(a, b) std::cout << (a) << ", " << (b) << "\n"
+#define LOG3(a, b, c) std::cout << (a) << ", " << (b) << ", " << (c) << "\n"
+#define LOG4(a, b, c, d) std::cout << (a) << ", " << (b) << ", " << (c) << ", " << (d) << "\n"
--- cpp/leetcode/playground.cpp
+++ cpp/leetcode/playground.cpp
@@ -1,25 +1,6 @@
-#include <algorithm>
-#include <cassert>
-#include <cctype>
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <stack>
-#include <queue>
-
-typedef long long int ll;
-typedef std::vector<int> vi;
-using std::pair;
-using std::queue;
-using std::stack;
-using std::string;
-using std::vector;
-#define FORN(i, n) for (int i = 0; i < (n); i++)
-#define FORN1(i, n) for (int i = 1; i < (n); i++)
-#define LOG(a) std::cout << (a) << "\n"
-#define LOG2(a, b) std::cout << (a) << ", " << (b) << "\n"
-#define LOG3(a, b, c) std::cout << (a) << ", " << (b) << ", " << (c) << "\n"
-// const ll MOD = ll(1e9 + 7);
+#if defined(RUNNING_LOCALLY)
+#include "local-stuff.hpp"
+#endif
 
 class Solution {
  public:
