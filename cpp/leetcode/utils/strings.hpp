3506e3d676bcc9255b5594b3fc159c0ea1f34ac4
Ayman Eltemsahi <aymaneltemsahi@hotmail.com>
2022-08-26 09:54:27 +0200

    update split_string function

--- cpp/leetcode/utils/strings.hpp
+++ cpp/leetcode/utils/strings.hpp
@@ -8,15 +8,16 @@ using std::string;
 using std::vector;
 
 vector<string> split_string(const string& value, string delimiter) {
-  size_t pos = 0;
-  string copy = value;
   vector<string> res;
-  while ((pos = copy.find(delimiter)) != string::npos) {
-    res.push_back(copy.substr(0, pos));
-    copy.erase(0, pos + delimiter.size());
+  size_t start = 0, end = value.find(delimiter);
+  while (end != string::npos) {
+    res.push_back(value.substr(start, end - start));
+    start = end + delimiter.length();
+    end = value.find(delimiter, start);
   }
-  if (!copy.empty()) res.push_back(copy);
 
+  res.push_back(value.substr(start, end));
+  if (res.back() == "") res.pop_back();
   return res;
 }
 
@@ -25,7 +26,6 @@ string trim_string(const string& value) {
   while (i < value.size() && value[i] == ' ') i++;
   while (j >= 0 && value[j] == ' ') j--;
   if (i > j) return "";
-
 
   return value.substr(i, j - i + 1);
 }
