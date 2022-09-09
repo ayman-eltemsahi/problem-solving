00858bdb1ad2cc172069fb826c5ffb015df4b656
Ayman Eltemsahi <aymaneltemsahi@hotmail.com>
2022-09-09 18:39:42 +0200

    added UnionFindRank

--- cpp/leetcode/common/union-find.hpp
+++ cpp/leetcode/common/union-find.hpp
@@ -7,21 +7,53 @@ using std::vector;
 
 class UnionFind {
  public:
-  int n;
   vector<int> parent;
-  UnionFind(int n_) : n(n_), parent(vector<int>(n_, -1)) {}
-
-  int find_parent(int i) {
-    if (i == parent[i]) return i;
-    if (parent[i] == -1) return (parent[i] = i);
-    return (parent[i] = find_parent(parent[i]));
+  UnionFind(int n) : parent(n) {
+    for (int i = 0; i < n; i++) parent[i] = i;
   }
 
-  int make_union(int x, int y) {
-    int x_set = find_parent(x);
-    int y_set = find_parent(y);
-    parent[x_set] = y_set;
-    return y_set;
+  int find(int x) {
+    if (x == parent[x]) return x;
+    return (parent[x] = find(parent[x]));
+  }
+
+  int connect(int x, int y) {
+    int x_p = find(x);
+    int y_p = find(y);
+    return (parent[x_p] = y_p);
+  }
+};
+
+class UnionFindRank {
+ public:
+  vector<int> parent, rank;
+
+  UnionFindRank(int n) : parent(n), rank(n) {
+    for (int i = 0; i < n; i++) {
+      parent[i] = i;
+      rank[i] = 1;
+    }
+  }
+
+  int find(int x) {
+    if (parent[x] == x) return x;
+    return (parent[x] = find(parent[x]));
+  }
+
+  int connect(int x, int y) {
+    int rootX = find(x);
+    int rootY = find(y);
+
+    if (rootX == rootY) return rootX;
+    if (rank[rootX] < rank[rootY]) {
+      parent[rootX] = rootY;
+      rank[rootY] += rank[rootX];
+      return rootY;
+    } else {
+      parent[rootY] = rootX;
+      rank[rootX] += rank[rootY];
+      return rootX;
+    }
   }
 };
 
