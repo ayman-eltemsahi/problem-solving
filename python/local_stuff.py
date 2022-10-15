bef05637cfd42dc49fc5d33b1dfd04676201d598
Ayman Eltemsahi <aymaneltemsahi@hotmail.com>
2022-10-15 21:11:32 +0200

    minor

--- python/local_stuff.py
+++ python/local_stuff.py
@@ -1,6 +1,11 @@
+from collections import deque
 from typing import List
 from heapq import heapify, heappop, heappush
 from functools import cache, lru_cache
+
+import sys
+sys.setrecursionlimit(10000)
+
 
 class TreeNode:
   def __init__(self, x):
