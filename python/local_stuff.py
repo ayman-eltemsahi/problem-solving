b85e6a9c52063b33f99e2dc8244190966ea24897
Ayman Eltemsahi <aymaneltemsahi@hotmail.com>
2022-10-15 16:17:15 +0200

    adde TreeNode to python/local_stuff.py

--- python/local_stuff.py
+++ python/local_stuff.py
@@ -1,3 +1,9 @@
 from typing import List
 from heapq import heapify, heappop, heappush
 from functools import cache, lru_cache
+
+class TreeNode:
+  def __init__(self, x):
+    self.val = x
+    self.left = None
+    self.right = None
