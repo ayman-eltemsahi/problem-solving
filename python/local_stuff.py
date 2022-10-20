41fca689ece0c4fd1a944dde70c13bc15a9b56b5
Ayman Eltemsahi <aymaneltemsahi@hotmail.com>
2022-10-20 07:09:51 +0200

    added python/local_stuff parseTree

--- python/local_stuff.py
+++ python/local_stuff.py
@@ -1,5 +1,6 @@
 from collections import deque
-from typing import List
+from typing import List, Optional
+from math import floor, ceil, log2
 from heapq import heapify, heappop, heappush
 from functools import cache, lru_cache
 
@@ -12,3 +13,22 @@ class TreeNode:
     self.val = x
     self.left = None
     self.right = None
+
+def parseTree(v: List[int]):
+  if not v: return None
+  q = deque()
+  root = TreeNode(v[0])
+  q.append(root)
+  i = 1
+  N = len(v)
+  while i < N:
+    node = q.popleft()
+    if v[i]:
+      node.left = TreeNode(v[i])
+      q.append(node.left)
+    i += 1
+    if i < N and v[i]:
+      node.right = TreeNode(v[i])
+      q.append(node.right)
+    i += 1
+  return root
